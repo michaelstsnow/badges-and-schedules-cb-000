38fa270c365474cd84bb6ac04208e8e379213e27
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 08:37:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,3 @@
 # Write your code here.
-def conference_badges(names_list)
-  room=1;
-  names_list.each{ |name|
-    puts "Hello, #{name}! You'll be assigned to room #{room}!"
-    room +=1;
-  }
+def batch_badge_creator
 end
