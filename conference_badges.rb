0de1a79077b91e8c4f11ba4f092551a7443a7fbd
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 08:42:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,8 @@ end
 def assign_rooms(names)
   room = 1;
   names.each do
-    puts "Hello, #{names}! You'll be assigned to room 1!"    
+    puts "Hello, #{names}! You'll be assigned to room #{room}!"
+    room +=1;
   end
 end
 
