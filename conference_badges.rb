7095fc6ad8db9b2e8ff06bbb1bab336a55af9248
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 08:46:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,18 +5,8 @@ end
 def badge_maker
 end
 
-def assign_rooms(names)
-  room = 1;
-  assignments=[];
-  names.each do
-    assignments[room-1]="Hello, #{names}! You'll be assigned to room #{room}!";
-    room +=1;
-  end
-  return assignments
+def assign_rooms()
 end
 
-def printer(final_array)
-  final_array.each{ |line|
-    puts "#{line}"
-  }
+def printer()
 end
